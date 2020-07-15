0fc4d2da09688bbbc6ebaee96bfb52a60a9a2336
root <dubiels86@gmail.com>
2020-07-14 23:43:06 -0400

    Added configution to task module for mongodb

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: 'Task', schema: TaskSchema}])],
   controllers: [TasksController],
   providers: [TasksService]
 })
